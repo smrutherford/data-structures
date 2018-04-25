e0927e0c34c6ab055607ff3a2331c204abd45d2d
Stacey Rutherford <rutherford.stacey@gmail.com>
2018-04-25 11:47:19 -0700

    Satisfied first three tests of stack shared behavior test

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,18 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return size;
   };
 
   return someInstance;
