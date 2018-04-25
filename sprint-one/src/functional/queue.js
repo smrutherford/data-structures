30b761029b009da7fe754a885961eccf94b13743
Stacey Rutherford <rutherford.stacey@gmail.com>
2018-04-25 12:32:09 -0700

    Finished all specs within first two tests for queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var values = [];
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    size++;
+    values.push(value);
   };
 
   someInstance.dequeue = function() {
+    size--;
+    return values.pop();
   };
 
   someInstance.size = function() {
+    if (size < 0){
+      return 0;
+    } else {
+      return size;    
+    }
   };
 
   return someInstance;
