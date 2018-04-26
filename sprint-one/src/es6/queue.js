8c7f7408a3a672b6a0f8dd0aa113472de80b3d74
Stacey Rutherford <rutherford.stacey@gmail.com>
2018-04-25 22:18:39 -0700

    Completed tests for es6 pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.begin = 0;
+    this.end = 0;
+  }
+
+  size() {
+    return this.end - this.begin < 0 ? 0 : this.end - this.begin;
+  }
+
+  enqueue(value) {
+    this.storage[this.end] = value;
+    this.end++;
+  }
+
+  dequeue() {
+    var temp = this.storage[this.begin];
+    delete this.storage[this.begin];
+    this.begin++;
+    return temp;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.length = 0;
+  }
+
+  size() {
+    return this.length < 0 ? 0 : this.length;
+  }
+
+  push(value) {
+    this.length++;
+    this.storage[this.length] = value;
+  }
+
+  pop() {
+    var temp = this.storage[this.length];
+    delete this.storage[this.length];
+    this.length--;
+    return temp;
   }
 
 }
+
