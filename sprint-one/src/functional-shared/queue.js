8ad401a38f628db66e3afd86b879f4c0f4e57656
Stacey Rutherford <rutherford.stacey@gmail.com>
2018-04-25 17:48:20 -0700

    Satisfied all tests for functional-shared pattern (queue)

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {},
+    end: 0,
+    front: 0
+  };
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.end] = value;
+    this.end++;
+  },
+  dequeue: function() {
+    var temp = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    return temp;	
+  },
+  size: function(){
+    return this.end - this.front < 0 ? 0 : this.end - this.front;
+  }	
+};
 
 
