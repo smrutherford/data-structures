eaed692953020a0da87fe58d421a7989942fe3db
smrutherford <rutherford.stacey@gmail.com>
2018-04-26 21:24:13 -0700

    Completed tree data structure tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);  
 
   return newTree;
 };
@@ -11,9 +12,22 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  let node = Tree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target) {
+  this.visited  = true;
+  if (this.value === target) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].visited !== true) {
+       return this.children[i].contains(target);
+      }
+    }
+  }
+  return false;
 };
 
 
